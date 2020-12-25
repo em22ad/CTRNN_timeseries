0bb3d026d9c9eb910ddbcb3e74e4f7057b20e1d6
em22ad <em22ad@gmail.com>
2020-12-25 10:32:35 -0600

    Add files via upload

---
+++ MatDL/zoo/init_two_ctrnnm.m
@@ -0,0 +1,20 @@
+function [model, opt] = init_two_ctrnnm(N, K, layers_size, opt)
+
+    weightScale = 0.01;
+    biasScale = 0;
+
+    NL1 = layers_size(1);
+
+    model.wx1 = randn(N, NL1) * weightScale;
+    model.wh1 = randn(NL1, NL1) * weightScale;
+    model.b1 = randn(1, NL1) * biasScale;
+    model.wy = randn(NL1, K) * weightScale;
+    model.by = randn(1, K) * biasScale;
+    model.v = zeros(NL1,NL1);
+    
+    p = fieldnames(model);
+    for i = 1:numel(p)
+        opt.vgrads.(p{i}) = zeros(size(model.(p{i})));
+    end
+
+end
